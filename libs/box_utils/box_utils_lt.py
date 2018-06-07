6d980c573b8f847d97658af4dbb21b8cbc0fdfa4
lillythomas <lillyelizathomas@gmail.com>
2018-06-07 15:39:33 -0700

    Update box_utils_lt.py

--- libs/box_utils/box_utils_lt.py
+++ libs/box_utils/box_utils_lt.py
@@ -61,31 +61,66 @@ def order_points(pts):
         print(flat_points_list)
 
         return flat_points_list
-        
+
 def read_reorder(coordinates):
     print("input coordinates: ", coordinates)
-    flat_points_list = [item for sublist in coordinates for item in sublist]
-    print("points_list: ", flat_points_list)
     def chunks(l, n):
         for i in range(0, len(l), n):
             yield l[i:i+n]
-    pairs = list(zip(*[iter(flat_points_list)] * 2))
-    chunked = list(chunks(pairs, 4))
-    print("chunked: ", chunked)
-    print("pairs: ", pairs)
-    ordered_points_list = []
-    for chunk in chunked:
-        ordered_points_list_chunk = order_points(chunk)
-        ordered_points_list.append(ordered_points_list_chunk)
-    print("original points list: ", chunked)
-    print("ordered_points_list: ", ordered_points_list)
-    flat_ordered_points_list = [item for sublist in ordered_points_list for item in sublist]
-    pairs = list(zip(*[iter(flat_ordered_points_list)] * 2))
-    chunked = list(chunks(pairs, 4))
-    chunked_array = np.array(chunked, dtype=np.float32)
-    chunked_array_angle = np.append(chunked_array, flat_points_list[-1])
-    print("out_chunked_array_angle: ", np.array(chunked_array_angle, dtype=np.float32))
+    def chunks1(l, n):
+        return [l[i:i+n] for i in range(0, len(l), n)]
+    flat_points_list = [item for sublist in coordinates for item in sublist]
+    print("points_list: ", flat_points_list)
+    if len(flat_points_list) > 9:
+        print("multi-box")
+        flat_points_array = np.array(flat_points_list)
+        chunked = chunks1(flat_points_list, 9)
+        print("multi-box chunked: ", chunked)
+        chunked_pts = []
+        for chunk in chunked:
+            pairs = list(zip(*[iter(chunk)] * 2))
+            chunked_pts.append(pairs)
+        print("chunked points: ", chunked_pts)
+        chunked1 = list(chunks(chunked_pts, 4))
+        ordered_points_list = []
+        for chunk1 in chunked_pts:
+            ordered_points_list_chunk = order_points(chunk1)
+            ordered_points_list.append(ordered_points_list_chunk)
+        print("ordered_points_list: ", ordered_points_list)
+        ordered_points_list_wClass = []
+        for chunk2, chunk0 in zip(ordered_points_list, chunked):
+            print("ordered_points_list chunk: ", chunk2)
+            print("category: ", chunk0[-1])
+            category = int(chunk0[-1])
+            chunk2 = chunk2.append(category)
+            ordered_points_list_wClass.append(chunk2)
+        print("original points list: ", chunked_pts)
+        print("ordered_points_list_wClass: ", ordered_points_list)
+        chunked_array_angle = ordered_points_list
+    else:
+        print("single box")
+        pairs = list(zip(*[iter(flat_points_list)] * 2))
+        chunked = list(chunks(pairs, 4))
+        print("chunked: ", chunked)
+        print("pairs: ", pairs)
+        ordered_points_list = []
+        for chunk in chunked:
+            ordered_points_list_chunk = order_points(chunk)
+            ordered_points_list.append(ordered_points_list_chunk)
+        print("original points list: ", chunked)
+        print("ordered_points_list: ", ordered_points_list)
+        flat_ordered_points_list = [item for sublist in ordered_points_list for item in sublist]
+        pairs = list(zip(*[iter(flat_ordered_points_list)] * 2))
+        print("ordered_points_list_pairs: ", pairs)
+        chunked = list(chunks(pairs, 4))
+        print("ordered_points_list_pairs_chunked: ", chunked)
+        chunked_array = np.array(chunked, dtype=np.float32)
+        print("out chunked_array: ", chunked_array)
+        chunked_array_angle = np.append(chunked_array, flat_points_list[-1])
+        print("chunked_array_angle: ", chunked_array_angle)
+        print("out_chunked_array_angle: ", np.array(chunked_array_angle, dtype=np.float32))
     return np.array(chunked_array_angle, dtype=np.float32)
+
 
 def clip_boxes_to_img_boundaries(decode_boxes, img_shape):
     '''
